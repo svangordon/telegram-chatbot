87580217f4378efae2b36e8443529ccb7a2bc9a4
Stephen Van Gordon <stephen.van.gordon@gmail.com>
2016-09-16 14:39:13 -0600

    trying to get chat logs to output how i want them to

--- src/chatMethods.js
+++ src/chatMethods.js
@@ -17,28 +17,40 @@ const createChatLog = () => {
   return Message.find({})
     .sort({date: 1})
     .exec((err, messages) => {
-      //console.log('found messages', messages);
-      const output = messages.map((msg) => {
-        return {
-          date: moment.unix(msg.date).format(),
-          from: msg.from.id,
-          username: msg.from.username,
-          text: msg.text
-        }
-      }).map((msg) => {
-        return "\n" + "From:" + " " + msg.username + "\n" + msg.text + "\n" + msg.date + "\n" + "=========";
-      }).join('');
-    // bot.sendMessage(msg.from.id, output);
+      // is this doing anything? don't think so...
+    //   //console.log('found messages', messages);
+    //   const output = messages.map((msg) => {
+    //     return {
+    //       date: moment.unix(msg.date).format(),
+    //       from: msg.from.id,
+    //       username: msg.from.username,
+    //       text: msg.text
+    //     }
+    //   }).map((msg) => {
+    //     return "\n" + "From:" + " " + msg.username + "\n" + msg.text + "\n" + msg.date + "\n" + "=========";
+    //   }).join('');
+    // // bot.sendMessage(msg.from.id, output);
   });
 };
 
-// createChatLog();
+const formatChatLog = (chatArr) => {
+  return chatArr.map((msg) => {
+    return {
+      date: moment.unix(msg.date).format(),
+      from: msg.from.id,
+      username: msg.from.username,
+      text: msg.text
+    }
+  }).map((msg) => {
+    return "\n" + "From:" + " " + msg.username + "\n" + msg.text + "\n" + msg.date + "\n" + "=========";
+  }).join('');
+}
 
 const getChatLog = (bot, msg) => {
   console.log(createChatLog());
   createChatLog().then((chatLog) => {
     console.log('getchatlog', chatLog);
-    bot.sendMessage(msg.from.id, chatLog);
+    bot.sendMessage(msg.from.id, formatChatLog(chatLog));
   });
 }
 
