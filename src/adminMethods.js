8bf6981213dcf5a5589aae6fabfb7626d534088e
Stephen Van Gordon <stephen.van.gordon@gmail.com>
2016-09-16 09:08:54 -0600

    added a generic command handler, and some handlers for admin commands

---
+++ src/adminMethods.js
@@ -0,0 +1,80 @@
+const Command = require('models.js').Command;
+
+// Admin
+
+// true if msg comes from an authorized user (the client, basically)
+const authorizeMsg = () => {
+  // TODO: check authorization
+  return true
+};
+
+const setCommand = (command, type) => {
+  command = {
+    name: command[0].split(' ')[1],
+    resp: command[0].split(' ').slice(2).join(' '),
+    type: type
+  };
+  Command.findOne({'name': command.name}, (err, foundCommand) => {
+    if (err) {
+      //TODO: some kind of error handling
+    } else if (foundCommand) {
+      Object.assign(foundCommand, command);
+      foundCommand.save((err) => {
+        // TODO: Success handler, i guess? I don't like having to pass around fromId like this
+      });
+    } else {
+      const newCommand = new Command();
+      Object.assign(newCommand, command);
+      newCommand.save();
+    }
+  });
+};
+
+const handleError = (err, fromId) => {
+  const errs = {
+    saveCommand: "Sorry, I couldn't save that command. Please try again."
+  };
+  bot.sendMessage(fromId, errs[err]);
+};
+
+const getCommands = () => { //
+  return Command.find({}).exec((err, commands) => {
+    if (err) {
+      console.log('getCommands err:', err);
+    }
+    return commands;
+  });
+}
+
+const genericCommandHandler = (bot, msg, commandName) => { // I'm pretty sure that I need to be passing bot here, or else binding it somewhere else
+  const fromId = msg.from.id;
+  Command.findOne({'name', commandName}, (err, command) => {
+    if (err) {
+      console.log('error', err);
+    }
+    if (command) {
+      if (command.type === "text") {
+        bot.sendMessage(fromId, resp);
+      } else {
+        bot.sendPhoto(fromId, resp);
+      }
+    }
+  });
+};
+
+const adminCallbacks = {
+  setcommand: setCommand
+};
+
+const adminCommands = [
+  'setcommand'
+];
+
+module.exports = {
+  authorizeMsg: authorizeMsg,
+  setCommand: setCommand,
+  getCommands: getCommand,
+  adminCallbacks: adminCallbacks,
+  handleError: handleError,
+  adminCommands: adminCommands
+};
--- src/server.js
+++ src/server.js
@@ -10,119 +10,46 @@ var token = '256121999:AAErcXm0hy-WKttT2uyQ7pjIkCjJsK6JP3s';
 // Setup polling way
 var bot = new TelegramBot(token, {polling: true});
 
-var userMessages = {};
+const adminMethods = require('./adminMethods.js');
 
-var commands = [/\/reciprocal/, /\/save/, /\/speak/];
+const commands = adminMethods.getCommands();
 
-const createMenu = () => {
-  return "Menu \n Stewed frogs \n pickled bats";
-};
-
-// true if msg comes from an authorized user (the client, basically)
-const authorizeMsg = () => {
-  // TODO: check authorization
-  return true
-};
-
-const parseSetCommandMsg = (msg) => {
-  console.log('parsing msg', msg);
-  const parsedMsg = {
-    name: msg[0].split(' ')[1],
-    resp: msg[0].split(' ').slice(2).join(' ')
-  };
-  console.log('parsed msg ==', parsedMsg);
-  return parsedMsg;
-};
-
-const setCommand = command => {
-  Command.findOne({'name': command.name}, (err, foundCommand) => {
-    if (err) {
-      //TODO: some kind of error handling 
-    } else if (foundCommand) {
-      Object.assign(foundCommand, command);
-      foundCommand.save((err) => {
-        // TODO: Success handler, i guess? I don't like having to pass around fromId like this
-      });
-    } else {
-      const newCommand = new Command();
-      //Object.assign(newCommand, command);
-      newCommand.name = command.name;
-      newCommand.type = command.type;
-      newCommand.resp = command.resp;
-      newCommand.save();
-    }
-  });
-
-};
-
-
-const handleError = (err, fromId) => {
-  const errs = {
-    saveCommand: "Sorry, I couldn't save that command. Please try again."
-  };
-  bot.sendMessage(fromId, errs[err]);
-}
-
-// // Matches /echo [whatever]
-bot.onText(/\/reciprocal (\d+)$/, function (msg, match) {
-  var fromId = msg.from.id;
-  var resp = 1 / parseInt(match[1]);
-  bot.sendMessage(fromId, resp);
-});
-
-bot.onText(/\menu/, function (msg) {
-  var fromId = msg.from.id;
-  var resp = "Commands: \n \/remember to save a message \n \/remind to repeat a message \n Or, send me any message to see it reversed.";
-  bot.sendMessage(fromId, resp);
-});
-
-bot.onText(/\/remember (.+$)/, function (msg, match) {
-  var fromId = msg.from.id;
-  userMessages[fromId] = match[1];
-  bot.sendMessage(fromId, "Got it. Tell me to \/remind you later.");
-});
-
-bot.onText(/\/remind/, function (msg) {
-  var fromId = msg.from.id;
-  var message = userMessages[fromId];
-  var resp = message ? "Earlier, you asked me to remember this message:\n" + message : "I'm sorry, I can't remember your message.";
-  bot.sendMessage(fromId, resp);
-});
-
-// TODO: write the image one of these
-bot.onText(/\/setcommand .+/, (msg, match) => {
-  if (authorizeMsg(msg)) {
-    const commandData = parseSetCommandMsg(match);
-    commandData.type = "text";
-    console.log('setting command', commandData);
-    setCommand(commandData);
-  }
-});
-
-//bot.onText(/^([^\/].+)/, function (msg, match) {
-//  var fromId = msg.from.id;
-//  var resp = match[0].split('').reverse().join('');
-//  bot.sendMessage(fromId, resp);
-//})
+// // TODO: write the image one of these
+// bot.onText(/\/setcommand .+/, (msg, match) => {
+//   if (authorizeMsg(msg)) {
+//     setCommand(match, "text");
+//   }
+// });
 
 bot.onText(/^.+$/, (msg, match) => {
   var fromId = msg.from.id;
-  if (commands.some((cur) => {
-    console.log('evaluating', cur);
-    return cur.test(match);
-  })) {
-    // the user used a command, so don't do anything.
-    // Actually, use this spot to write the message to the db
-  } else {
-    bot.sendMessage(fromId, createMenu());
+  if (match.match(/^\/.+? /)) { // a command has been found
+    const command = match.match(/^\/.+? /)[0].slice(1, -1); // get the command, drop / and trailing space
+    if (adminMethods.adminCommands.indexOf(command) !== -1) {
+      // It's an admin command
+      // TODO: add authentication
+      adminMethods.adminCallbacks[command](bot, msg);
+    } else {
+      // It's not an admin command
+      adminMethods.genericCommandHandler(bot, msg, command);
+    }
+  } else { // no command has been found
+    bot.sendMessage(fromId, "Imagine this is the menu");
   }
+  // for (let i = 0; i < commands.length; i++) { // check all of our commands to see if any match. possibly needs to be streamlined (maybe just check for /)
+  //   if (match.indexOf('/' + commands[i].name) !== -1) { // we've found a command; check if it's an admin command
+  //     if (adminMethods.adminCallbacks[i]) {
+  //       adminMethods.adminCallbacks[i](msg)
+  //     } else {
+  //       adminMethods.genericCommandHandler(msg);
+  //     }
+  //   }
+  // }
+  // if (commands.some((cur) => {
+  //   return match.indexOf(cur.name) !== -1;
+  // })) {
+  //   adminMethods.commandCallbacks[]
+  // } else {
+  //   bot.sendMessage(fromId, createMenu());
+  // }
 });
-
-// Any kind of message
-//bot.on('message', function (msg) {
-//  var fromId = msg.from.id;
-//  console.log('bang');
-//  if (msg.text.indexOf('/reciprocal') === -1) {
-//    bot.sendMessage(fromId, msg.text.split('').reverse().join(''))
-//  }
-//});
